3041bbd7a09e5b3f5da468f07ca805c2e0b222fe
npatilraghu <npatilraghu@gmail.com>
2023-10-06 08:05:49 +0530

    first commit

---
+++ src/main/java/com/rnp/blog/blogappapi/BlogAppApiApplication.java
@@ -0,0 +1,16 @@
+package com.rnp.blog.blogappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApiApplication.class, args);
+				System.out.println("Om Sai Ram...");
+
+		System.out.println("Blog App API is running...");
+	}
+
+}
